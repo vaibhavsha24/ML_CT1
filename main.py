f88459763b844e4c9dfca1ed2526100d9add517b
vaibhavsha24 <49113916+vaibhavsha24@users.noreply.github.com>
2022-08-31 15:56:49 +0530

    31 aug 2022

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+import librosa
+import matplotlib.pyplot as plt
+from scipy.io import wavfile as wav
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    audio_file_path="D:\\1st sem\\ML\\CT\\Actor_01\\03-01-01-01-02-02-01.wav"
+    librosa_audio_data,librosa_sample_rate=librosa.load(audio_file_path)
+    print(librosa_audio_data)
+    plt.figure(figsize=(12,4))
+    plt.plot(librosa_audio_data)
+    plt.show()
+    wave_sample_rate, wave_audio = wav.read(audio_file_path)
+    wave_audio
